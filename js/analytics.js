6241f734ab3e83c8e7cc41521b62cb4dcaadff47
BJ Beier <bjbeier@gmail.com>
2026-05-25 18:16:19 -0400

    feat: conditionally set Google Analytics code based on hostname

--- js/analytics.js
+++ js/analytics.js
@@ -1,11 +1,15 @@
 window.loadTracking = function () {
+    // Production (bjb) uses G-5R4EJK544M, staging (bjb-staging) or local dev uses G-393461309
+    const isProduction = window.location.hostname === 'bjbeier.com' || window.location.hostname === 'www.bjbeier.com';
+    const gaId = isProduction ? 'G-5R4EJK544M' : 'G-393461309';
+
     const script = document.createElement('script');
     script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-5R4EJK544M";
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
     document.head.appendChild(script);
 
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
-    gtag('config', 'G-5R4EJK544M');
+    gtag('config', gaId);
 };
